176fa158efce6e663a5bb314dde7903c94652b57
heroca60 <heroca60@gmail.com>
2021-03-01 18:42:36 -0600

    SRP, OCP

---
+++ src/solid/Draw.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid;
+
+
+/**
+ *
+ * @author hendrick
+ */
+public class Draw {
+
+    /* public void dibujar(Personaje personaje){
+    
+        Violando el principio OCP                 
+        if (personaje instanceof Goku){
+            System.out.println("Pintando a Gokú");
+        }else if(personaje instanceof Vegeta){
+            System.out.println("Pintando a Vegeta");
+        }    
+     */
+    public void dibujar(IDraw personaje) {
+        personaje.dibujar();
+    }
+
+    /*
+    public void dibujar2(List<IDraw> drawPersonaje){
+        drawPersonaje.forEach(d -> {
+            d.dibujar();
+        });
+    }
+     */
+}
---
+++ src/solid/Goku.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid;
+
+/**
+ *
+ * @author hendrick
+ */
+public class Goku extends Personaje{
+    
+
+    public Goku() {
+    }
+    
+            
+}
